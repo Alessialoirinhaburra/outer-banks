1dbbc2fafa58aac8cb5bc725b12644b9f0802816
Alessialoirinhaburra <163419098+Alessialoirinhaburra@users.noreply.github.com>
2024-03-15 10:07:38 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function mudarimagemfora()
+{
+    document.getElementById("imagem").src ="https://febreteen.com.br/wp-content/uploads/2021/04/Outer-Banks.jpg";
+}
+
+function mudarimagemdentro()
+{
+    document.getElementById("imagem").src ="https://hips.hearstapps.com/hmg-prod/images/outerbanks-s2-1627669422.jpg?crop=0.566xw:1.00xh;0.199xw,0&resize=640:*";
+}
